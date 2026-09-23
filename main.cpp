aef709577bfefbcc642a62e0c41c19a9d2dcf4a1
Austyn <jacopelandalvnc@gmail.com>
2026-09-23 16:55:02 -0400

    Add initial ASCII cube rotation and rendering
    - Implement X, Y, and Z rotation calculations
    - Add perspective projection and depth buffering
    - Render the first cube face using ASCII characters
    - Add an animation loop that updates rotation angles

--- main.cpp
+++ main.cpp
@@ -1,16 +1,77 @@
-#include <iostream>
+#include <cstring>
+#include <math.h>
+#include <stdio.h>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+float A, B, C;
+
+float cubeWidth = 10;
+int width = 160, height = 44;
+float zBuffer[160 * 44];
+char buffer[160 * 44];
+int backgroundASCIICode = ' ';
+int distanceFromCam = 60;
+float k1 = 40;
+
+float incrementSpeed = 0.6;
+
+float x,y,z;
+float ooz;
+int xp, yp;
+int idx;
+
+float calculateX(int i, int j, int k) {
+    return j * sin(A) * sin(B) * cos(C) - k  * cos(A) * sin(B) * cos(C) +
+        j * cos(A) * sin(C) + k * sin(A) * sin(C) + i * cos(B) * cos(C);
+}
+
+float calculateY(int i, int j, int k) {
+    return j * cos(A) * cos(C) + k * sin(A) * cos(C) -
+        j * sin(A) * sin(B) * sin(C) + k * cos(A) * sin(B) * sin(C) -
+            i * cos(B) * sin(C);
+}
+
+float calculateZ(int i, int j, int k) {
+    return k * cos(A) * cos(B) - j * sin(A) * cos(B) + i * sin(B);
+}
+
+void calculateForSurface(float cubeX, float cubeY, float cubeZ, int ch) {
+    x = calculateX(cubeX, cubeY, cubeZ);
+    y = calculateY(cubeX, cubeY, cubeZ);
+    z = calculateZ(cubeX, cubeY, cubeZ);
+
+    ooz = 1/z;
+
+    xp = (int)(width/2 + K1 * ooz * x * 2);
+    yp = (int)(height/ 2 + K1 * ooz * y);
+
+    idx = xp + yp * width;
+    if (idx>= 0 && idx < width * height) {
+        if (ooz > zBuffer[idx]) {
+            zBuffer[idx] = ooz;
+            buffer[idx] = ch;
+        }
     }
+}
 
-    return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
+int main() {
+printf("\x1b[2J");
+    while (1) {
+        memset(buffer, backgroundASCIICode, width * height);
+        memset(zBuffer, 0, width * height * 4 );
+        for (float cubeX = -cubeWidth; cubeX < cubeWidth; cubeX += incrementSpeed) {
+            for (float cubeY = -cubeWidth; cubeY < cubeWidth;
+                cubeY += incrementSpeed) {
+            calculateForSurface(cubeX, cubeY, -cubeWidth, '#') ;
+            }
+        }
+        printf("\x1b[H");
+        for ( int k = 0; k < width * height; k++) {
+            putchar(k % width ? buffer[k] : 10);
+        }
+
+        A += 0.005;
+        B += 0.005;
+    }
+    return  0;
 }
